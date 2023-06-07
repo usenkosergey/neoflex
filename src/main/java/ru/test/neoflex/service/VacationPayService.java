9ad724b72d90a5003abf9edeba8e7b3767422709
Sergey Usenko <s.usenko@goodt.me>
2023-06-07 20:36:38 +0700

    commit4

--- src/main/java/ru/test/neoflex/service/VacationPayService.java
+++ src/main/java/ru/test/neoflex/service/VacationPayService.java
@@ -25,7 +25,6 @@ public class VacationPayService {
 
         log.info("----");
         log.info("----");
-        log.info("----");
         return vacationPay.setScale(2, RoundingMode.FLOOR);
     }
 }
